460d692300f6b671d6c1194cf1f936a43d69df34
AKSHAYA.G <akshaya26.inn@gmail.com>
2025-12-26 18:48:46 +0530

    Add files via upload

---
+++ employee details.java
@@ -0,0 +1,20 @@
+// employee details (class and object)
+
+class employee
+{
+    int id = 26;
+    String name = "Akshaya";
+
+void show()
+{
+    System.out.println(id);
+    System.out.println(name);
+}
+
+    public static void main (String args[])
+    {
+     employee e = new employee();
+        e.show();
+    }
+}
+
